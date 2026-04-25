cc68e21b0e2ac66073d56410010337ef5a4f7935
rahulsingh2007 <rahulsingh70815@gmail.com>
2026-04-25 13:59:59 +0545

    About Variables learned

---
+++ 2-Variables/index.js
@@ -0,0 +1,20 @@
+/* let x=5;
+console.log(x); */
+/* let age = 25;
+console.log(`You are ${age} years old.`);
+console.log(typeof(age)); */
+
+// Boolean
+/* let online = false;
+let forSale = true;
+let isStudent = true;
+console.log(`Bro is online: ${online}`);
+console.log(`Is this cal for sale: ${forSale}`);
+console.log(`Enrolled: ${isStudent}`); */
+
+let fullName = "Rahul Singh";
+let age = 25;
+let student = true;
+document.getElementById(`p1`).textContent = `Your name is ${fullName}.`;
+document.getElementById(`p2`).textContent = `You are ${age} years old.`;
+document.getElementById(`p3`).textContent = `Enrolled: ${student}`;
