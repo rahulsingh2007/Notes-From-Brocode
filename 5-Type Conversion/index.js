8aedcdadef6ac4a76ad8acb3bbf2f0baea3a40f5
rahulsingh2007 <rahulsingh70815@gmail.com>
2026-04-26 07:11:55 +0545

    How to convert ype of a value done

--- 5-Type Conversion/index.js
+++ 5-Type Conversion/index.js
@@ -1,2 +1,18 @@
 /* TYPE CONVERSION = CHANGE THE DATATYPE OF A VALUE TO ANOTHER
 (STRINGS TO NUMBERS OR BOOLEAN) */
+
+/* let age = window.prompt("How old are you?")
+age = Number(age)
+age += 1;
+console.log(age, typeof age);12 */
+
+let x = "Pizza";
+let y = "Pizza";
+let z = "Pizza";
+
+x = Number(x);
+y = String(y);
+z = Boolean(z);
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
