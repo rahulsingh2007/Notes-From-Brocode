0ab6db983ff1a22fe81129e0d78dd5a19d6f4593
rahulsingh2007 <rahulsingh70815@gmail.com>
2026-04-26 07:44:28 +0545

    Math and its properties

---
+++ 8-Math/index.js
@@ -0,0 +1,20 @@
+// MATH = BUILT-IN OBJECT THAT PROVIDES A COLLECTION OF PROPERTIES AND METHODS
+
+// console.log(Math.PI);
+
+let x = 3;
+let y = 2;
+let z = 1;
+
+// z = Math.round(x);
+// z = Math.floor(x);
+// z = Math.ceil(x);
+// z = Math.trunc(x);
+// z = Math.pow(y ,x);
+// z = Math.sqrt(x);
+// z = Math.log(x);
+/* let x = 3;
+let y = 2;
+let z = 1;
+let max = Math.max(x, y, z)
+console.log(max); */
