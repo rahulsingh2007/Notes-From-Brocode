07c61db5c51e2effb16cf938a20836b3b6764851
rahulsingh2007 <rahulsingh70815@gmail.com>
2026-04-26 07:21:09 +0545

    How to use Const

---
+++ 6-Const/index.js
@@ -0,0 +1,13 @@
+// CONST = A VARIABLE THAT CAN'T BE CHANGED
+
+const PI = 3.14159;
+let radius;
+let circumference;
+
+
+document.getElementById(`mySubmit`).onclick = function () {
+    radius = document.getElementById(`myText`).value;
+    radius = Number(radius);
+    circumference = 2 * PI * radius;
+    document.getElementById(`myH3`).textContent = "The circumference of circle is " + circumference + "cm";
+};
