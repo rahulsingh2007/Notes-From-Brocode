238d352f95d78156c3c29a39a3af91aa1b065c1a
rahulsingh2007 <rahulsingh70815@gmail.com>
2026-05-10 17:00:50 +0545

    Some string methods

---
+++ 14-String Methods/script.js
@@ -0,0 +1,24 @@
+// STRING METHODS = ALLOW YOU TO MANIPULATE AND WORK WITH TEXT (STRINGS)
+
+/* let userName = "Brocode";
+
+console.log(userName.charAt(5));
+console.log(userName.indexOf("o"));
+console.log(userName.lastIndexOf("o"));
+console.log(userName.length);
+console.log(userName.trim());
+console.log(userName.repeat(5)); */
+
+/* let result = userName.startsWith(" ")
+console.log(result);
+if(result){
+    console.log("Your usernmae can't begin with ''.");
+}
+else{
+    console.log(userName);
+} */
+
+let phoneNumber = "123-456-7890"
+// phoneNumber = phoneNumber.replaceAll("-","")
+// phoneNumber=phoneNumber.padStart(20, "0")?
+console.log(phoneNumber);
