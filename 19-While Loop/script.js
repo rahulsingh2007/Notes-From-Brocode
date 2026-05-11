7c8ee41dfa2c47ea8efa7748eb012fbbc9d0e5b0
rahulsingh2007 <rahulsingh70815@gmail.com>
2026-05-11 08:12:51 +0545

    while and do while loop

---
+++ 19-While Loop/script.js
@@ -0,0 +1,41 @@
+// WHILE LOOP = REPEAT SOME CODE WHILE SOME CONDITION IS TRUE
+
+/* let username = "";
+while (username === "" || username === null) {
+    username = window.prompt('Enter you name.');
+}
+
+console.log(`Hello ${username}`);
+
+do {
+    username = window.prompt('Enter you name.');
+} while (username === "" || username === null);
+ */
+
+let loggedIn = false;
+let username;
+let password;
+
+/* while (!loggedIn) {
+    username = window.prompt("Enter you username.");
+    password = window.prompt("Enter your password.")
+    if (username === "myUsername" && password === "myPassword") {
+        loggedIn = true;
+        console.log("You are logged in.");
+    }
+    else {
+        console.log('Invalid Credentials! Please try again.');
+    }
+} */
+
+/* do {
+    username = window.prompt("Enter you username.");
+    password = window.prompt("Enter your password.")
+    if (username === "myUsername" && password === "myPassword") {
+        loggedIn = true;
+        console.log("You are logged in.");
+    }
+    else {
+        console.log('Invalid Credentials! Please try again.');
+    }
+} while (!loggedIn); */
