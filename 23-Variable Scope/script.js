2d9809c550ca607d7cd981791562b4eb9c47e9f7
rahulsingh2007 <rahulsingh70815@gmail.com>
2026-05-11 18:28:56 +0545

    Variable scope (local or global) in js

---
+++ 23-Variable Scope/script.js
@@ -0,0 +1,12 @@
+// VARIABLE SCOPE = WHERE A VARIABLE IS RECOGNIZED AND ACCESSIBLE (LOCAL OR GLOBAL)
+
+// let x = 3;
+function function1() {
+    let x = 1;
+    console.log(x);
+}
+function function2() {
+    let x = 2;
+    console.log(x);
+}
+function2();
