749b0ee257cf844e23948594632115becec7336c
rahulsingh2007 <rahulsingh70815@gmail.com>
2026-05-11 07:31:16 +0545

    Logical operator in JS

---
+++ 17-Logical Operators/script.js
@@ -0,0 +1,33 @@
+// LOGICAL OPERATORS = USED TO COMBINE OR MANIPULATE BOOLEAN VALUES (TRUE OR FALSE)
+//                      AND = &&
+//                      OR = ||
+//                      NOT = !
+
+const temp = 200;
+
+/* 
+// AND OPERATOR    
+    if(temp > 0 && temp <=30){
+    console.log("The weather is goood.");
+}
+else{
+    console.log("The weather is bad.");
+}
+ */
+
+/* 
+// OR OPERATOR 
+    if(temp <= 0 || temp > 30){
+    console.log("The weather is bad.");
+}
+else{
+    console.log("The weather is good.");
+} */
+
+const isSunny = true;
+if(!isSunny){
+    console.log("It is CLOUDY.");
+}
+else{
+    console.log("It is SUNNY.");
+}
