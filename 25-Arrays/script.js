bf58222d85630864e8a99868278e5bd223325ab5
rahulsingh2007 <rahulsingh70815@gmail.com>
2026-05-12 21:10:24 +0545

    Arrays in JS

---
+++ 25-Arrays/script.js
@@ -0,0 +1,23 @@
+// ARRAY = A VARIABLE LIKE STRUCTURE THAT CNA HOLD MORE THAN 1 VALUE
+
+let fruits = ["apple","orange","banana","coconut"];
+
+/* fruits.push("coconut")
+fruits.pop()
+fruits.unshift("mango")
+fruits.shift()
+console.log(fruits[0]);
+console.log(fruits[1]);
+console.log(fruits[2]);
+console.log(fruits[3]); */
+
+/* let numOfFruits = fruits.length;
+console.log(numOfFruits);
+
+let index = fruits.indexOf("orange")
+console.log(index);
+ */
+
+for (let i = 0; i < fruits.length; i++) {
+    console.log(fruits[i]);
+}
