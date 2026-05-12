152ff00651ae8925a6cb74c9d391e8ff10b8bcd6
rahulsingh2007 <rahulsingh70815@gmail.com>
2026-05-12 21:17:32 +0545

    Arrays in JS

--- 25-Arrays/script.js
+++ 25-Arrays/script.js
@@ -17,7 +17,7 @@ console.log(numOfFruits);
 let index = fruits.indexOf("orange")
 console.log(index);
  */
-fruits.sort().reverse
+fruits.sort()
 
 for (let i = 0; i < fruits.length; i++) {
     console.log(fruits[i]);
