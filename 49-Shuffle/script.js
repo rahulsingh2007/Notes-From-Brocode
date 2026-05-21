b74b9b39de57d3b104ab9ec2729cd4de2b34feb3
rahulsingh2007 <rahulsingh70815@gmail.com>
2026-05-21 07:50:14 +0545

    Shuffle Cards

---
+++ 49-Shuffle/script.js
@@ -0,0 +1,18 @@
+// const cards = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'];
+// cards.sort(() => Math.random() - 0.5); Not reccomended
+// console.log(cards);
+
+//------------------------- Fisher-Yates Algorithm ------------------------
+
+const cards = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'];
+
+shuffle(cards);
+
+console.log(cards);
+
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const random = Math.floor(Math.random() * (i + 1));
+        [array[i], array[random]] = [array[random], array[i]];
+    }
+}
