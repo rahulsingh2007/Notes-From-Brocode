7287611545400a2dc71fd4b04f5de439f9922f15
rahulsingh2007 <rahulsingh70815@gmail.com>
2026-08-03 08:08:11 +0545

    Memory Leaks

---
+++ 83-Memory Leaks/script.js
@@ -0,0 +1,11 @@
+let count = 0;
+const int = setInterval(() => {
+    if (count < 10) {
+        count++;
+        console.log(count);
+    }
+    else {
+        clearInterval(int);
+        console.log("Finished!");
+    }
+}, 500);
